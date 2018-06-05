e36dc3abc80c098ab68bf9bb730082aa1ef566ec
Leon Lu <yanglu@microsoft.com>
2018-06-05 10:42:38 -0700

    start: Building an Integration with Visual Studio Team Services API
    module 3.4

---
+++ Controllers/VSTeamAPIController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TutorialVSTSAPI1.Controllers
+{
+    public class VSTeamAPIController : Controller
+    {
+        // GET: BasicAuth
+        [HttpGet]
+        public ActionResult BasicAuth()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TutorialVSTSAPI1.Startup))]
+namespace TutorialVSTSAPI1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

9cacac5763274e0008a7ff4c0194bb9b7d13e35b
Leon Lu <yanglu@microsoft.com>
2018-06-05 10:58:07 -0700

    demo: Code Web Application
    module 3.5

--- Controllers/VSTeamAPIController.cs
+++ Controllers/VSTeamAPIController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace TutorialVSTSAPI1.Controllers
 {
@@ -13,6 +12,38 @@ namespace TutorialVSTSAPI1.Controllers
         public ActionResult BasicAuth()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> BasicAuth(string username, string password, string account)
+        {
+            return View(viewName: "VSTSAPIResponse", model: await GetProjects(username, password, account));
+        }
+
+        private async Task<string> GetProjects(string username, string password, string account)
+        {
+            string responseBody = string.Empty;
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", username, password))));
+                    using (HttpResponseMessage response = client.GetAsync(
+                        $"https://{account}.visualstudio.com/DefaultCollection/_apis/api-version=1.0").Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        responseBody = await response.Content.ReadAsStringAsync();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                responseBody = $"{{\"Error\":\"{ex.Message}\"}}";
+            }
+
+            return responseBody;
         }
     }
 }
